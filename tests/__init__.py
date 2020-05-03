8eee245032e7585bf41cd89a59c4da37f8266663
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-05-02 23:30:49 -0300

    Added tests for Disc Manager

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,10 @@
 import unittest
 import tests.src.test_cli as test_cli
+import tests.src.test_disc_manager as test_disc_manager
 
 def load_tests(loader, tests, pattern):
   suite = unittest.TestSuite()
   suite.addTests(loader.loadTestsFromModule(test_cli))
+  suite.addTests(loader.loadTestsFromModule(test_disc_manager))
 
   return suite
