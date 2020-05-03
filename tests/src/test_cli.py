bf9902c979c90eeac9fa8bcc420f788b897da425
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-05-03 02:55:56 -0300

    Refactoring CLI tests

--- tests/src/test_cli.py
+++ tests/src/test_cli.py
@@ -3,36 +3,43 @@ import src.cli as cli
 
 class TestCli(unittest.TestCase):
 
-  def test_process_arguments(self):
-    expected = {
-      "test1": "testendjend =jndjend",
-      "test2": "=jrfnrjfnjr",
-      "test4": " teste"
-    }
-    arguments = cli.process_arguments([
-      "test1='testendjend =jndjend'",
-      "test2==jrfnrjfnjr",
+  def setUp(self):
+    cli.arguments_map = cli.process_arguments([
+      "test1='value =2'",
+      "Test1='whatever",
+      "test2==value",
+      "-test2==whatever",
+      "2test2==whatever",
+      "test-quotes=\"value\"",
       "test3=",
+      "test4=' value'",
       " kndeknde",
-      "test4=' teste'"
+      "-test1",
+      "-Test4",
+      "-1test",
     ])
 
-    self.assertEqual(arguments, expected)
+  def tearDown(self):
+    cli.arguments_map = {}
+
+  def test_process_arguments(self):
+    expected = {
+      "test-quotes": "value",
+      "test1": "value =2",
+      "test2": "=value",
+      "test4": " value",
+      "-test1": True
+    }
+
     self.assertEqual(cli.get_arguments(), expected)
 
   def test_get_argument(self):
-    cli.arguments_map = cli.process_arguments([
-      "test1='testendjend =jndjend'",
-      "test2==jrfnrjfnjr",
-      "test3=",
-      " kndeknde",
-      "test4=' teste'"
-    ])
-
-    self.assertEqual("testendjend =jndjend", cli.get_argument("test1"))
-    self.assertEqual("=jrfnrjfnjr", cli.get_argument("test2"))
-    self.assertEqual(" teste", cli.get_argument("test4"))
+    self.assertEqual("value =2", cli.get_argument("test1"))
+    self.assertEqual("=value", cli.get_argument("test2"))
+    self.assertEqual(" value", cli.get_argument("test4"))
     self.assertIsNone(cli.get_argument("test3"))
+    self.assertIsNone(cli.get_argument("-test3"))
+    self.assertTrue(cli.get_argument("-test1"))
 
 if __name__ == "__main__":
   unittest.main()
