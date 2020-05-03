4bdc76953526ff95d43d9ff136abb07ff8095282
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-05-03 04:21:07 -0300

    Added tests for Utils

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,11 +2,13 @@ import unittest
 import tests.src.test_cli as test_cli
 import tests.src.test_disc_manager as test_disc_manager
 import tests.src.test_params as test_params
+import tests.src.test_utils as test_utils
 
 def load_tests(loader, tests, pattern):
   suite = unittest.TestSuite()
   suite.addTests(loader.loadTestsFromModule(test_cli))
   suite.addTests(loader.loadTestsFromModule(test_disc_manager))
   suite.addTests(loader.loadTestsFromModule(test_params))
+  suite.addTests(loader.loadTestsFromModule(test_utils))
 
   return suite
