ac25c05ac9a6123649c26d0b8bf74faa95d004c0
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-05-04 19:26:20 -0300

    Added tests for function normalize_str

--- tests/src/test_utils.py
+++ tests/src/test_utils.py
@@ -14,5 +14,12 @@ class TestUtils(unittest.TestCase):
     self.assertEqual(False, utils.parse_int('', False))
     self.assertEqual(False, utils.parse_int(None, False))
 
+  def test_normalize_str(self):
+    self.assertEqual("abc", utils.normalize_str("abc\n"))
+    self.assertEqual("abcabc", utils.normalize_str("abc\nabc"))
+    self.assertIsNone(utils.normalize_str(None))
+    self.assertTrue(utils.normalize_str(True))
+    self.assertFalse(utils.normalize_str(False))
+
 if __name__ == "__main__":
   unittest.main()
