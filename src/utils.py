ce0cdc898fb11e78abe78589507066b48548b00e
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-05-04 19:25:33 -0300

    Added normalize_str function

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import re
+
 def parse_int(value, fallback=0):
   """
   Convert a string number to integer
@@ -9,3 +11,12 @@ def parse_int(value, fallback=0):
     return int(value)
   except ValueError:
     return fallback
+
+def normalize_str(value: str):
+  """
+  Just remove unnecessary characters from value
+  """
+  if type(value) != str:
+    return value
+
+  return re.sub(r'\n', lambda m: '', value)
