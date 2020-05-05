cfeebc45c538b556c32fc49ceff70933a0fab853
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-05-04 23:30:13 -0300

    Added extract_extension and pattern_test functions

--- src/utils.py
+++ src/utils.py
@@ -20,3 +20,24 @@ def normalize_str(value: str):
     return value
 
   return re.sub(r'\n', lambda m: '', value)
+
+def extract_extension(path: str):
+  """
+  Returns the file extension, otherwise return None
+  """
+  if type(path) != str:
+    return None
+
+  value_regex = re.compile("\.([\w\d]+?)$")
+  matches = value_regex.search(path)
+  return matches.group(1) if matches else None
+
+def pattern_test(pattern: str, value: str):
+  """
+  Just fire the pattern in value and return a boolean
+  """
+  if type(pattern) != str or type(value) != str:
+    return False
+
+  value_regex = re.compile(pattern)
+  return bool(value_regex.search(value))
